8bd3c58685ee6a98753e701a02fea6282856eedb
Andre Zimpel <az@yum.de>
2014-04-17 23:18:02 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Satiisfy::Application.config.secret_key_base = 'ae21beb73f53e77c88a03a594a8acb526719dcc32ef9c412c389893164a03deaffa16be91fcd9131e16c8c6ebb8ddb0bb22dbfcf0e9f6125d97ceaf539e0fcb2'
