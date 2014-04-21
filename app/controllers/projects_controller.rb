10bdd7284be138f8aa4700dec829448ec89a164b
Andre Zimpel <az@yum.de>
2014-04-21 19:01:11 +0200

    display active state

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,6 +11,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
+    @questions = @project.questions.order("title ASC")
   end
 
   # GET /projects/new
