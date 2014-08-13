522d8243ba25c1fcafc2c15cca79dd76a7d46b12
Andre Zimpel <az@yum.de>
2014-08-13 17:55:15 +0200

    paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,41 @@
 class User < ActiveRecord::Base
+  # userstamps
+  model_stamper
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable,
   devise :invitable, :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, :invitable
 
+  # associations
   belongs_to :account
-  has_one :profile
 
-
-  # userstamps
-  model_stamper
-
-  accepts_nested_attributes_for :profile
-
-
-  # create profile after creating a user
-  after_create :create_profile
-  def create_profile
-    p = Profile.new
-    p.user_id = self.id
-    p.save
-  end
+  # paperclip
+  has_attached_file :avatar,
+    :styles => {
+      :extra_large => "1200x1200>",
+      :large => "600x600>",
+      :medium => "300x300>",
+      :thumb => "150x150>",
+      :tiny => "75x75>",
+      :extra_large_square => "1200x1200#",
+      :large_square => "600x600#",
+      :medium_square => "300x300#",
+      :thumb_square => "150x150#",
+      :tiny_square => "75x75#"
+    },
+    :default_url => "/assets/default_avatar.jpg"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 
   # get user "Name"
   def fullname
-    if self.profile
-      if self.profile.firstname != nil && self.profile.lastname != nil
-        return self.profile.firstname + " " + self.profile.lastname
-      end
-      return self.email
-    end
-    return self.email
+    # if self.profile
+    #   if self.profile.firstname != nil && self.profile.lastname != nil
+    #     return self.profile.firstname + " " + self.profile.lastname
+    #   end
+    #   return self.email
+    # end
+    # return self.email
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140810101843) do
+ActiveRecord::Schema.define(version: 20140813155400) do
 
   create_table "accounts", force: true do |t|
     t.string   "title"
@@ -110,6 +110,10 @@ ActiveRecord::Schema.define(version: 20140810101843) do
     t.string   "invited_by_type"
     t.integer  "creator_id"
     t.integer  "updater_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
